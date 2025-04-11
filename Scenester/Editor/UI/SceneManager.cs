81224289607b941d056edb7f474c345964f27189
Matin Parsapour <matin.parsapour.iam@gmail.com>
2025-04-11 13:31:31 +0330

    Update scene manager to save

--- Scenester/Editor/UI/SceneManager.cs
+++ Scenester/Editor/UI/SceneManager.cs
@@ -1,10 +1,25 @@
+using System.Collections;
+using System.Threading;
+using Dastan.Scenester.Editor.Entity.Base;
+using Dastan.Scenester.Editor.Services;
+using Dastan.Scenester.Editor.Services.Impl;
 using Dastan.Scenester.Editor.UI.Btn;
 using Dastan.Scenester.Editor.UI.SceneDirector.Bar;
+using UnityEditor;
+using UnityEngine;
 
 namespace Dastan.Scenester.Editor.UI
 {
+    
+    
     public class SceneManager
-    {
+    { 
+        private static readonly ScenarioSaveAgentService ScenarioAgent = new ScenarioSaveAgentService();
+        private static readonly DialogueSaveAgentService DialogueAgent = new DialogueSaveAgentService();
+        
+        private static float threadProgress;
+        private static bool done;
+        
         public static void AddScene(ScenarioButtonContainer scenarioButtonContainer)
         {
             TabBar.GetInstance(null).Add(scenarioButtonContainer);
@@ -18,6 +33,30 @@ namespace Dastan.Scenester.Editor.UI
         public static int SizeScene(ScenarioButtonContainer scenarioButtonContainer)
         {
             return TabBar.GetInstance(scenarioButtonContainer).childCount;
+        }
+
+        public static IEnumerator Save(Scenario scenario)
+        {
+            try
+            {
+                EditorUtility.DisplayProgressBar("Saving...", "Working...", 0);
+                bool success = ScenarioAgent.Save(scenario);
+                EditorUtility.DisplayProgressBar("Saving...", "Working...", 10);
+                if (success)
+                {
+                    foreach (Dialogue dialogue in scenario.GetDialogues())
+                    {
+                        DialogueAgent.Save(dialogue);
+                    }
+                }
+                yield return null;
+                EditorUtility.DisplayProgressBar("Saving...", "Working...", 100);
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+
         }
     }
 }
